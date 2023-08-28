1a1875443560d35b70986218af679177fc150011
Genevive-Mbesi <genevive.opondo@student.moringaschool.com>
2023-08-28 15:15:26 +0300

    review

---
+++ review.py
@@ -0,0 +1,21 @@
+class Review:
+    _all_reviews = []
+
+    def __init__(self, customer, restaurant, rating):
+        self._customer = customer
+        self._restaurant = restaurant
+        self._rating = rating
+        self._all_reviews.append(self)
+
+    def rating(self):
+        return self._rating
+
+    @classmethod
+    def all(cls):
+        return cls._all_reviews
+
+    def customer(self):
+        return self._customer
+
+    def restaurant(self):
+        return self._restaurant
